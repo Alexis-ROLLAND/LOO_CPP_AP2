cdd6b22dfd4a83ff7c3830bf0abbdf3968c78810
Alexis ROLLAND <alexis.rolland@univ-tours.fr>
2023-01-17 12:32:00 +0100

    last changes - almost ok for AP

--- include/Sinus.hpp
+++ include/Sinus.hpp
@@ -74,54 +74,202 @@ class SinusComputeHelper{
 		~SinusComputeHelper() = default;
 };
 
-	
+/**
+ *	Sinus class (standalone version)
+ */ 	
 class Sinus{
 	private:
-	// Internal stuff
-	mutable std::vector<SignalPoint>	tabSignal{};	/**< Vector containing the signal data (points)	*/
+	
+	mutable std::vector<SignalPoint>	tabSignal{};	/**< Vector containing the signal data (points)	- mutable because needs to be calculated, even with const objects	*/
 		
 	ComputeParameters	SimulParams{defaulttStart, defaulttStop, defaultNbPoints};	/**< Simulation parameters */		
 	SinusParam Parameters{defaultA0, defaultAmplitude, defaultOmega, defaultPhi0};	/**< Sinus parameters */
 	
-	mutable	bool	needToRecompute{true};
+	mutable	bool	needToRecompute{true};											/**< Flag set if the signal needs to be "recomputed" (change of params)	*/ 
 		
-	SinusComputeHelper	helper{};
+	SinusComputeHelper	helper{};													/**< Composition of the helper class		*/
+	
+	/**
+	 * Compute (private) method
+	 * 		links to the Compute public method of the helper 
+	 * 
+	 * const : callable with a const object  
+	 * noexcept : this method throws no exception
+	 * 
+	 */
 	void	compute() const noexcept {this->tabSignal = this->helper.Compute(this->Parameters, this->SimulParams);};
 	
 	// private accessors
-	bool	getNeedToRecompute() const noexcept {return this->needToRecompute;};
+	[[nodiscard]] bool	getNeedToRecompute() const noexcept {return this->needToRecompute;};
 	void	setNeedToRecompute(bool value) const noexcept {this->needToRecompute = value;};
 	
 	public:
 	// SMF
-	Sinus() = default ;
-	Sinus(const SinusParam &SinusParameters, const ComputeParameters &SimulParameters);	// Ctor with params - has to check the params, throws exception in case of bad parameter
-	virtual ~Sinus() = default;
+	Sinus() = default ;						/**< Default Ctor	*/
+	Sinus(const SinusParam &SinusParameters, const ComputeParameters &SimulParameters);	/**< 	 Ctor with params - has to check the params, throws exception in case of bad parameter	*/
+	virtual ~Sinus() = default;				/**< Default Dtor	*/
+	
+	Sinus(const Sinus&) = default;			/**< Default Copy Ctor	*/
+	Sinus& operator=(const Sinus&) = default;	/**< Default copy assignment operator	*/
+	
+	Sinus(Sinus&&) noexcept = default;			/**< Default move Ctor	*/
+	Sinus& operator=(Sinus&&) = default;		/**< Default move assignment operator	*/
 	
 	
-	// Accessors
-	float 	getA0()const  noexcept {return this->Parameters.A0;};
+	
+	// Public Accessors
+	/**
+	 * getter (public) for the A0 parameter 
+	 * 
+	 * const : callable with a const object  
+	 * noexcept : this method throws no exception
+	 * 
+	 * @return value of A0 parameter (inside the Parameters structure)
+	 * [[nodiscard]] : the return value should not be discarded (compiler warning)
+	 */
+	[[nodiscard]] float 	getA0()const  noexcept {return this->Parameters.A0;};
+	
+	/**
+	 * setter (public) for the A0 parameter (inside the Parameters structure)
+	 * 
+	 * @param[in]	A0	Value of A0
+	 * noexcept : this method throws no exception
+	 */
 	void	setA0(float A0) noexcept {this->Parameters.A0 = A0;setNeedToRecompute(true);};
 	
-	float 	getAmplitude()const  noexcept {return(this->Parameters.Amplitude);};
-	void	setAmplitude(float Amplitude)  noexcept {this->Parameters.Amplitude = Amplitude;setNeedToRecompute(true);};
-		
-	float 	getOmega()const noexcept{return(this->Parameters.Omega);};
+	/**
+	 * getter (public) for the Amplitude parameter 
+	 * 
+	 * const : callable with a const object  
+	 * noexcept : this method throws no exception
+	 * 
+	 * @return value of Amplitude parameter (inside the Parameters structure)
+	 * [[nodiscard]] : the return value should not be discarded (compiler warning)
+	 */
+	[[nodiscard]] float 	getAmplitude()const  noexcept {return(this->Parameters.Amplitude);};
+	
+	/**
+	 * setter (public) for the A0 parameter (inside the Parameters structure)
+	 * 
+	 * @param[in]	Amplitude	Value of Amplitude
+	 * noexcept : this method throws no exception
+	 */
+	void	setAmplitude(float Amplitude) noexcept {this->Parameters.Amplitude = Amplitude;setNeedToRecompute(true);};
+	
+	/**
+	 * getter (public) for the Omega parameter 
+	 * 
+	 * const : callable with a const object  
+	 * noexcept : this method throws no exception
+	 * 
+	 * @return value of Omega parameter (rad/s) (inside the Parameters structure)
+	 * [[nodiscard]] : the return value should not be discarded (compiler warning)
+	 */	
+	[[nodiscard]] float 	getOmega()const noexcept{return(this->Parameters.Omega);};
+	
+	/**
+	 * setter (public) for the Omega parameter (inside the Parameters structure)
+	 * 		This value must be >= 0
+	 * @param[in]	_Omega	Value of Omega
+	 * @throw 	std::domain_error("Omega can’t be negative.") if the given parameter is negative
+	 */
 	void	setOmega(float _Omega){if (_Omega < 0) throw std::domain_error("Omega can’t be negative.") ;this->Parameters.Omega = _Omega;setNeedToRecompute(true);};
 		
-	float	getPhi0() const noexcept {return this->Parameters.Phi0;};
+	/**
+	 * getter (public) for the Phi0 parameter 
+	 * 
+	 * const : callable with a const object  
+	 * noexcept : this method throws no exception
+	 * 
+	 * @return value of Phi0 parameter (rad, [-2pi; +2pi] range) (inside the Parameters structure)
+	 * [[nodiscard]] : the return value should not be discarded (compiler warning)
+	 */		
+	[[nodiscard]] float	getPhi0() const noexcept {return this->Parameters.Phi0;};
+	
+	/**
+	 * setter (public) for the Phi0 parameter (inside the Parameters structure)
+	 * 		if the given parameter is outside the [-2pi; +2pi] range, it'll be shrinked to fit. 
+	 * 
+	 * @param[in]	_Phi0	Value of Phi0
+	 * noexcept : this method throws no exception
+	 */
 	void	setPhi0(float _Phi0) noexcept {this->Parameters.Phi0 = std::fmod(_Phi0 , (2*pi) );setNeedToRecompute(true);};
 	
-	float	gettStart() const noexcept {return this->SimulParams.tStart;};
+	/**
+	 * getter (public) for the tStart parameter 
+	 * 
+	 * const : callable with a const object  
+	 * noexcept : this method throws no exception
+	 * 
+	 * @return value of tStart parameter (inside the SimulParams structure)
+	 * [[nodiscard]] : the return value should not be discarded (compiler warning)
+	 */	
+	[[nodiscard]] float	gettStart() const noexcept {return this->SimulParams.tStart;};
+	
+	/**
+	 * setter (public) for the tStart parameter (inside the SimulParams structure)
+	 * 		tStart must be >= 0 and < tStop. 
+	 * 
+	 * @param[in]	_tStart		Value of tStart
+	 * 
+	 * @throw 	std::domain_error("tStart can't be negative.") if the given parameter is negative.
+	 * @throw 	std::overflow_error("tStart can’t be greater or equal than tStop.") if the given parameter is >= than tStop 
+	 */
 	void	settStart(float _tStart); 
 	
-	float 	gettStop()const  noexcept {return this->SimulParams.tStop;};
+	/**
+	 * getter (public) for the tStop parameter 
+	 * 
+	 * const : callable with a const object  
+	 * noexcept : this method throws no exception
+	 * 
+	 * @return value of tStop parameter (inside the SimulParams structure)
+	 * [[nodiscard]] : the return value should not be discarded (compiler warning)
+	 */	
+	[[nodiscard]] float 	gettStop()const  noexcept {return this->SimulParams.tStop;};
+	
+	/**
+	 * setter (public) for the tStop parameter (inside the SimulParams structure)
+	 * 		tStop must be > 0 and > tStart
+	 * 	  
+	 * @param[in]	_tStop		Value of tStop
+	 * 
+	 * @throw 	std::domain_error("tStop can’t be negative or null.") if the given parameter is negative or nul.
+	 * @throw 	std::underflow_error("tStop can’t be lesser than tStart.") if the given parameter is < than tStart 
+	 */
 	void	settStop(float _tStop);
 		
-	unsigned int	getNbPoints()const  noexcept  {return this->SimulParams.nbPoints;};
+	/**
+	 * getter (public) for the nbPoints parameter 
+	 * 
+	 * const : callable with a const object  
+	 * noexcept : this method throws no exception
+	 * 
+	 * @return value of nbPoints (inside the SimulParams structure)
+	 * [[nodiscard]] : the return value should not be discarded (compiler warning)
+	 */		
+	[[nodiscard]] unsigned int	getNbPoints()const  noexcept  {return this->SimulParams.nbPoints;};
+	
+	/**
+	 * setter (public) for the nbPoints parameter (inside the SimulParams structure)
+	 * 		nbPoints must be > 0
+	 * 	  
+	 * @param[in]	_nbPoints		Value of tStop
+	 * 
+	 * @throw 	td::invalid_argument("nbPoints must be greater than 0.") if the given parameter is negative or nul.
+	 */
 	void	setNbPoints(unsigned int _nbPoints) {if (_nbPoints == 0) throw std::invalid_argument("nbPoints must be greater than 0.");this->SimulParams.nbPoints = _nbPoints;setNeedToRecompute(true);};
 	
-	unsigned int	generate(const std::filesystem::path FileName=defaultFileName)const;
+	/**
+	 * 	generate method
+	 * 		
+	 * 	@param[in] 	FileName(=defaultFileName)	The complete path of the data file to write.	
+	 * 
+	 * 	@return : Size of the generated file (bytes)
+	 * 
+	 * @throw  std::ios::exceptions 
+	 */
+	unsigned int	generate(const std::filesystem::path FileName=defaultFileName)const;	// return value can be discarded 
 	
 };
 
--- src/sinus_main_test.cpp
+++ src/sinus_main_test.cpp
@@ -5,8 +5,47 @@
 #include "doctest.h"
 //----------------------------------------------------------------------
 
+#define	TEST0_PRJ_ARCHITECTURE							/**< Project architecture only - may be disabled once passed	*/
+
+#define	TEST1_ACCESSORS_NON_CONST_OBJECT
+
+#define	TEST2_GLOBAL
+#define	TEST21_SETTER_A0_AND_AMPLITUDE
+#define	TEST22_SETTER_OMEGA
+#define	TEST23_SETTER_PHI0
+#define	TEST24_SETTER_TSTART
+#define	TEST25_SETTER_TSTOP
+#define	TEST26_SETTER_NBPOINTS
+
+#define	TEST3_DEFAULT_CONST_OBJECT
+
+#define	TEST4_GLOBAL
+#define	TEST41_HELPER_COMPUTE_NON_CONST
+#define	TEST42_HELPER_COMPUTE_CONST
+
+#define	TEST5_GLOBAL
+#define	TEST51_GENERATE_NON_CONST_DEFAULT_FILENAME
+#define	TEST52_GENERATE_NON_CONST_STD_FILENAME
+
+#define	TEST6_GLOBAL
+#define	TEST61_PARAM_CTOR_GOOD_VALUES_NON_CONST_OBJECT
+#define	TEST62_PARAM_CTOR_GOOD_VALUES_CONST_OBJECT
+#define	TEST63_PARAM_CTOR_BAD_OMEGA
+#define	TEST64_PARAM_CTOR_BAD_PHI0
+#define	TEST65_PARAM_CTOR_BAD_TSTART
+#define	TEST66_PARAM_CTOR_BAD_TSTOP
+#define	TEST67_PARAM_CTOR_BAD_NBPOINTS
+
+
 #include "Sinus.hpp"
 
+#ifdef	TEST0_PRJ_ARCHITECTURE
+TEST_CASE("0 - Project architecture"){
+	std::cout << "Project Architecture is OK\n";
+}
+#endif	/*	TEST0_PRJ_ARCHITECTURE */
+
+#ifdef	TEST1_ACCESSORS_NON_CONST_OBJECT
 TEST_CASE("1 - Testing the Sinus class : Default non const object and getters") {
 	Sinus mySinus{};
 	
@@ -18,11 +57,13 @@ TEST_CASE("1 - Testing the Sinus class : Default non const object and getters") 
 	CHECK(mySinus.gettStop() == doctest::Approx(defaulttStop));
 	CHECK(mySinus.getNbPoints() == defaultNbPoints);	
 }
+#endif	/* EST1_ACCESSORS_NON_CONST_OBJECT	*/
 
-
+#ifdef	TEST2_GLOBAL
 TEST_CASE("2 - Testing the Sinus class : Default non const object and setters") {
 	Sinus mySinus{};
 	
+	#ifdef	TEST21_SETTER_A0_AND_AMPLITUDE
 	// Setter for A0
 	float	A0 = 2.5;
 	mySinus.setA0(A0);
@@ -32,7 +73,9 @@ TEST_CASE("2 - Testing the Sinus class : Default non const object and setters") 
 	float 	Amplitude = 3.3;
 	mySinus.setAmplitude(Amplitude);
 	CHECK(mySinus.getAmplitude() == doctest::Approx(Amplitude));
+	#endif	/* TEST21_SETTER_A0_AND_AMPLITUDE	*/
 	
+	#ifdef	TEST22_SETTER_OMEGA
 	// Setter fo Omega
 	float 	Omega = 4*pi;
 	mySinus.setOmega(Omega);
@@ -40,7 +83,9 @@ TEST_CASE("2 - Testing the Sinus class : Default non const object and setters") 
 	
 	Omega = -Omega;		// if Omega < 0...
 	CHECK_THROWS_WITH_AS(mySinus.setOmega(Omega), "Omega can’t be negative.", std::domain_error);	// ...check exception
+	#endif	/* TEST22_SETTER_OMEGA */
 	
+	#ifdef	TEST23_SETTER_PHI0
 	// Setter for Phi0
 	float 	Phi0 = pi / 2;
 	mySinus.setPhi0(Phi0);
@@ -52,7 +97,9 @@ TEST_CASE("2 - Testing the Sinus class : Default non const object and setters") 
 	Phi0 = -pi/2;
 	mySinus.setPhi0(Phi0 - 2*pi);	// if Phi0 < -2*pi...
 	CHECK(mySinus.getPhi0() == doctest::Approx(Phi0));	// check Phi0 is now > -2*pi
+	#endif	/* TEST23_SETTER_PHI0 */
 	
+	#ifdef	TEST24_SETTER_TSTART
 	// Setter for tStart
 	REQUIRE(mySinus.gettStop() == doctest::Approx(defaulttStop));	// tStop must be at is default value (1)
 	float tStart = 0.5;
@@ -64,7 +111,9 @@ TEST_CASE("2 - Testing the Sinus class : Default non const object and setters") 
 	CHECK_THROWS_WITH_AS(mySinus.settStart(tStart),"tStart can’t be greater or equal than tStop.",std::overflow_error); // ...check exception
 	tStart = mySinus.gettStop();	// if tStart == tStop
 	CHECK_THROWS_WITH_AS(mySinus.settStart(tStart),"tStart can’t be greater or equal than tStop.",std::overflow_error); // ...check exception
+	#endif	/* TEST24_SETTER_TSTART */
 	
+	#ifdef	TEST25_SETTER_TSTOP
 	// Setter for tStop
 	tStart = 0.5;
 	mySinus.settStart(tStart);
@@ -80,15 +129,20 @@ TEST_CASE("2 - Testing the Sinus class : Default non const object and setters") 
 	CHECK_THROWS_WITH_AS(mySinus.settStop(tStop),"tStop can’t be lesser than tStart.",std::underflow_error);	// ...check exception
 	tStop = mySinus.gettStart();	// if tStop == tStart
 	CHECK_THROWS_WITH_AS(mySinus.settStop(tStop),"tStop can’t be lesser than tStart.",std::underflow_error);	// ...check exception
+	#endif	/* TEST25_SETTER_TSTOP */
 	
+	#ifdef	TEST26_SETTER_NBPOINTS
 	// Setter for nbPoints
 	unsigned int	nbPoints = 200;
 	mySinus.setNbPoints(nbPoints);
 	CHECK(mySinus.getNbPoints() == nbPoints);
 	nbPoints = 0;	// if nbPoints == 0 	(can't be < 0, because is unsigned)
 	CHECK_THROWS_WITH_AS(mySinus.setNbPoints(nbPoints),"nbPoints must be greater than 0.",std::invalid_argument);
+	#endif	/* TEST26_SETTER_NBPOINTS */
 }
+#endif	/* TEST2_GLOBAL	*/
 
+#ifdef	TEST3_DEFAULT_CONST_OBJECT
 TEST_CASE("3 - Testing the Sinus class : Default const object and getters") {
 	const Sinus mySinus{};
 	
@@ -100,13 +154,16 @@ TEST_CASE("3 - Testing the Sinus class : Default const object and getters") {
 	CHECK(mySinus.gettStop() == doctest::Approx(defaulttStop));
 	CHECK(mySinus.getNbPoints() == defaultNbPoints);	
 }
+#endif	/* TEST3_DEFAULT_CONST_OBJECT 	*/
 
+#ifdef	TEST4_GLOBAL
 TEST_CASE("4 - Testing the compute function global bahaviour (not the real algorithm) , within a compute helper class"){
 	
 	SinusParam	testSinusParam{defaultA0, defaultAmplitude, defaultOmega, defaultPhi0};
 	ComputeParameters	testSimulParams{defaulttStart, defaulttStop, defaultNbPoints};
 	std::vector<SignalPoint>	resSinus{};
 	
+	#ifdef	TEST41_HELPER_COMPUTE_NON_CONST
 	SinusComputeHelper	helper{};
 	
 	resSinus = helper.Compute(testSinusParam, testSimulParams);
@@ -115,8 +172,9 @@ TEST_CASE("4 - Testing the compute function global bahaviour (not the real algor
 	CHECK(resSinus.size() == defaultNbPoints);	// is the result vector the correct size ?
 	SignalPoint Point = resSinus.front();	// get first element of result vector
 	CHECK( Point.tn == doctest::Approx(testSimulParams.tStart));	// is the tn matching start time ?
+	#endif	/* TEST41_HELPER_COMPUTE_NON_CONST */
 	
-	
+	#ifdef	TEST42_HELPER_COMPUTE_CONST
 	const SinusComputeHelper	chelper{};
 	resSinus = chelper.Compute(testSinusParam, testSimulParams);
 		
@@ -124,9 +182,11 @@ TEST_CASE("4 - Testing the compute function global bahaviour (not the real algor
 	CHECK(resSinus.size() == defaultNbPoints);	// is the result vector the correct size ?
 	Point = resSinus.front();	// get first element of result vector
 	CHECK( Point.tn == doctest::Approx(testSimulParams.tStart));	// is the tn matching start time ?
-	
+	#endif	/* TEST42_HELPER_COMPUTE_CONST	*/
 }
+#endif	/* TEST4_GLOBAL	*/
 
+#ifdef	TEST5_GLOBAL
 TEST_CASE("5 - Testing the generate method - non const objects"){
 	Sinus mySinus{};
 	
@@ -135,13 +195,16 @@ TEST_CASE("5 - Testing the generate method - non const objects"){
 	REQUIRE(std::filesystem::exists(FileName) == false);
 	unsigned int 		expectedSize{1850};
 	
+	#ifdef	TEST51_GENERATE_NON_CONST_DEFAULT_FILENAME
 	mySinus.generate();
 	
 	std::filesystem::file_status fs =  std::filesystem::status(FileName);
 	CHECK(std::filesystem::exists(FileName));
 	CHECK(fs.type() == std::filesystem::file_type::regular );	// Possible use of td::filesystem::is_regular_file instead
 	CHECK(std::filesystem::file_size(FileName) == expectedSize);
+	#endif	/* TEST51_GENERATE_NON_CONST_DEFAULT_FILENAME	*/
 	
+	#ifdef	TEST52_GENERATE_NON_CONST_STD_FILENAME
 	mySinus.setOmega(4*pi);
 	mySinus.setAmplitude(2);
 	FileName = "sinus2.data";
@@ -152,9 +215,11 @@ TEST_CASE("5 - Testing the generate method - non const objects"){
 	CHECK(std::filesystem::exists(FileName));
 	CHECK(fs.type() == std::filesystem::file_type::regular );	// Possible use of td::filesystem::is_regular_file instead
 	CHECK(std::filesystem::file_size(FileName) == expectedSize);
-	
+	#endif	/* TEST52_GENERATE_NON_CONST_STD_FILENAME */
 }
+#endif	/* TEST5_GLOBAL	*/
 
+#ifdef 	TEST6_GLOBAL
 TEST_CASE("6 - Testing the parametrized Ctor"){
 	float	testA0{-0.5};
 	float 	testAmplitude{3.3};
@@ -168,6 +233,8 @@ TEST_CASE("6 - Testing the parametrized Ctor"){
 	SinusParam	testSinusParam{testA0, testAmplitude, testOmega, testPhi0};
 	ComputeParameters	testSimulParams{testtStart, testtStop, testNbPoints};
 	
+	
+	#ifdef 	TEST61_PARAM_CTOR_GOOD_VALUES_NON_CONST_OBJECT
 	SUBCASE("Good values - non const object"){
 		Sinus mySinus(testSinusParam, testSimulParams);
 		
@@ -179,7 +246,9 @@ TEST_CASE("6 - Testing the parametrized Ctor"){
 		CHECK(mySinus.gettStop() == doctest::Approx(testtStop));
 		CHECK(mySinus.getNbPoints() == testNbPoints);	
 	}
+	#endif	/* TEST61_PARAM_CTOR_GOOD_VALUES_NON_CONST_OBJECT */
 	
+	#ifdef	TEST62_PARAM_CTOR_GOOD_VALUES_CONST_OBJECT
 	SUBCASE("Good values - const object"){
 		const Sinus mySinus(testSinusParam, testSimulParams);
 		
@@ -191,19 +260,25 @@ TEST_CASE("6 - Testing the parametrized Ctor"){
 		CHECK(mySinus.gettStop() == doctest::Approx(testtStop));
 		CHECK(mySinus.getNbPoints() == testNbPoints);	
 	}
+	#endif	/* TEST62_PARAM_CTOR_GOOD_VALUES_CONST_OBJECT */
 	
+	#ifdef	TEST63_PARAM_CTOR_BAD_OMEGA
 	SUBCASE("Bad value for Omega - const and non const object"){
 		testSinusParam.Omega = -testOmega;
 		CHECK_THROWS_WITH_AS(Sinus mySinus(testSinusParam, testSimulParams), "Omega can’t be negative.", std::domain_error);	// ...check exception
 		CHECK_THROWS_WITH_AS(const Sinus mySinus(testSinusParam, testSimulParams), "Omega can’t be negative.", std::domain_error);	// ...check exception
 	}
+	#endif	/* TEST63_PARAM_CTOR_BAD_OMEGA */
 	
+	#ifdef	TEST64_PARAM_CTOR_BAD_PHI0
 	SUBCASE("Out of bounds value for Phi0 - const and non const object"){
 		testSinusParam.Phi0 = testPhi0 + 3*pi;
 		CHECK_THROWS_WITH_AS(Sinus mySinus(testSinusParam, testSimulParams), "Initial phi0 can’t be outside bounds.", std::domain_error);	// ...check exception
 		CHECK_THROWS_WITH_AS(const Sinus mySinus(testSinusParam, testSimulParams), "Initial phi0 can’t be outside bounds.", std::domain_error);	// ...check exception
 	}
+	#endif	/* TEST64_PARAM_CTOR_BAD_PHI0 */
 	
+	#ifdef	TEST65_PARAM_CTOR_BAD_TSTART
 	SUBCASE("Bad values for tStart - const and non const object"){
 		testSimulParams.tStart = -testtStart;
 		CHECK_THROWS_WITH_AS(Sinus mySinus(testSinusParam, testSimulParams), "tStart can't be negative.", std::domain_error);	// ...check exception
@@ -212,9 +287,10 @@ TEST_CASE("6 - Testing the parametrized Ctor"){
 		testSimulParams.tStart = testtStop + testtStart;
 		CHECK_THROWS_WITH_AS(Sinus mySinus(testSinusParam, testSimulParams), "tStart can’t be greater or equal than tStop.",std::overflow_error);	// ...check exception
 		CHECK_THROWS_WITH_AS(const Sinus mySinus(testSinusParam, testSimulParams), "tStart can’t be greater or equal than tStop.",std::overflow_error);	// ...check exception
-		
 	}
+	#endif	/* TEST65_PARAM_CTOR_BAD_TSTART */
 	
+	#ifdef	TEST66_PARAM_CTOR_BAD_TSTOP
 	SUBCASE("Bad values for tStop - const and non const object"){
 		testSimulParams.tStop = -testtStop;
 		CHECK_THROWS_WITH_AS(Sinus mySinus(testSinusParam, testSimulParams), "tStop can’t be negative or null.", std::domain_error);	// ...check exception
@@ -229,15 +305,17 @@ TEST_CASE("6 - Testing the parametrized Ctor"){
 		CHECK_THROWS_WITH_AS(Sinus mySinus(testSinusParam, testSimulParams), "tStart can’t be greater or equal than tStop.",std::overflow_error);	// ...check exception
 		CHECK_THROWS_WITH_AS(const Sinus mySinus(testSinusParam, testSimulParams), "tStart can’t be greater or equal than tStop.",std::overflow_error);	// ...check exception
 	}
+	#endif	/* TEST66_PARAM_CTOR_BAD_TSTOP */
 	
+	#ifdef	TEST67_PARAM_CTOR_BAD_NBPOINTS
 	SUBCASE("Bad value nbPoints - const and non const object"){
 		testSimulParams.nbPoints = 0;
 		CHECK_THROWS_WITH_AS(Sinus mySinus(testSinusParam, testSimulParams), "nbPoints must be greater than 0.",std::invalid_argument);	// ...check exception
 		CHECK_THROWS_WITH_AS(const Sinus mySinus(testSinusParam, testSimulParams), "nbPoints must be greater than 0.",std::invalid_argument);	// ...check exception
 	}
-	
+	#endif	/* TEST67_PARAM_CTOR_BAD_NBPOINTS */
 	
 }
-
+#endif	/* TEST6_GLOBAL	*/
 
 
